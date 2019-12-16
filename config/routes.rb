99066f6cc53afb44a3e41080e7ad8e29944a69e7
kozimatakesi <riverbook708@gmail.com>
2019-12-16 20:03:28 +0900

    okay

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root    'tweets#index'
+  get           'tweets' => 'tweets#index'
+  get       'tweets/new' => 'tweets#new'
+  delete   'tweets/:id'  => 'tweets#destroy'
+  post          'tweets' => 'tweets#create'
+  get  'tweets/:id/edit' => 'tweets#edit'
+  patch    'tweets/:id'  => 'tweets#update'
+  get     'tweets/battle'=> 'tweets#battle'
+end
